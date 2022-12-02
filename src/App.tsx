7441ef874d44fcae3d7902ee76857c8214c65cd7
Gabi <gabi@MacPro-0169.local>
2022-12-02 15:54:42 -0300

    chore: updated UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,45 @@
-import { useCallback, useEffect, useState } from 'react';
-import './App.css';
-import { WalletInfo, WALLET_IDS } from './wallets/base';
-import { enable, getAvailableWallets, getBalance, getNetwork } from './walletsGateway';
+import { useEffect, useMemo, useState } from "react";
+import "./App.css";
+import { WalletInfo, WALLET_IDS } from "./wallets/base";
+import {
+  enable,
+  getAvailableWallets,
+  getBalance,
+  getNetwork,
+} from "./walletsGateway";
 
-function WalletCard(props: { wallet: WalletInfo, handleClick: Function }) {
+function WalletCard(props: {
+  wallet: WalletInfo;
+  selected: boolean;
+  onClick: () => void;
+}) {
   return (
-        <div className="p-8 bg-slate-800 hover:bg-slate-700 rounded-lg cursor-pointer flex h-full flex-col ">
-          <button className='flex flex-col' onClick={() => props.handleClick(props.wallet.id)}>
-            <div className="flex">
-                <img src={props.wallet.icon} alt='wallet-logo' width={28} height={28}/>
-                <span className='text-gray-400 ml-2 text-sm grow'>{`id: ${props.wallet.id}`}</span>
-            </div>
-            <h1 className="text-white font-bold line-clamp-1 mb-2 mt-4">{`Name: ${props.wallet.name}`}</h1>
-            <div className="flex mt-auto">
-                <p className="flex-1 text-gray-300 text-sm">{`api version: ${props.wallet.apiVersion}`}</p>
-            </div>
-            </button>
-        </div>
+    <div
+      className={`p-6 rounded-md bg-gray-900 transition-all shadow-lg cursor-pointer rounded-md ${
+        props.selected ? "border-txpink border-2" : ""
+      }`}
+      onClick={() => props.onClick()}
+    >
+      <div className="flex">
+        <img src={props.wallet.icon} alt="wallet-logo" width={28} height={28} />
+
+        <h3 className="text-xl font-medium tracking-wide ml-2">
+          {props.wallet.name}
+        </h3>
+      </div>
+      <div className="flex flex-col mt-4">
+        <p className="text-gray-300 text-sm">{`id: ${props.wallet.id}`}</p>
+        <p className="text-gray-300 text-sm">{`api version: ${props.wallet.apiVersion}`}</p>
+      </div>
+    </div>
   );
 }
 
 function App() {
   const [wallets, setWallets] = useState([] as WalletInfo[]);
+  const [selectedWallet, setSelectedWallet] = useState<
+    WALLET_IDS | undefined
+  >();
   const [enabledWallet, setEnabledWallet] = useState<WalletInfo>();
   const [balance, setBalance] = useState<string>();
   const [network, setNetwork] = useState<string>();
@@ -29,72 +47,86 @@ function App() {
 
   useEffect(() => {
     setWallets(getAvailableWallets());
-  },[]);
-
-  const connectWallet = useCallback( async (walletId: WALLET_IDS) => {
-    try {
-
-      // clears the error state
-      setError(undefined);
-
-      // Enables the wallet
-      setEnabledWallet(await enable(walletId));
-
-      // Gets the enabled wallet balance
-      setBalance(await getBalance());
-
-      // Gets the enabled wallet network
-      setNetwork(await getNetwork());
-
-    }catch(error: any) {
-      setError(error.message || 'unknown errorr');
-    }
   }, []);
 
+  useMemo(async () => {
+    try {
+      if (selectedWallet) {
+        // clears the error state
+        setError(undefined);
+
+        // Enables the wallet
+        setEnabledWallet(await enable(selectedWallet!));
+
+        // Gets the enabled wallet balance
+        setBalance(await getBalance());
+
+        // Gets the enabled wallet network
+        setNetwork(await getNetwork());
+      }
+    } catch (error: any) {
+      setError(error.message || "unknown error");
+    }
+  }, [selectedWallet]);
+
   return (
-    <div className="w-screen h-screen bg-gray-900 overflow-auto">
-        <div className="container max-w-6xl p-16  h-full w-full">
-            <header className="mb-3 py-6 w-full flex flex-col justify-between">
-                <div className='flex'>
-                    <img src="/logo.svg" className="mr-4 h-6" alt="TxPipe Logo" />
-                    <h2 className="text-m text-gray-400 font-normal">Starter Kit provided by TxPipe</h2>
-                </div>
-                
-                <h3 className="text-3xl text-gray-200 font-extrabold mt-4">Wallet Connector</h3>
-                <div className="mt-8 rounded-lg border border-blue-500 bg-blue-600 bg-opacity-10 p-4 text-white mb-4">
-                    <h1 className="font-bold">Connect to a Wallet</h1>
-                    <h3 className="text-sm text-blue-500 mt-2">Select which wallet to connect and perform basic interactions.</h3>
-                </div>
-            </header>
+    <div className="bg-gray-950 h-screen w-screen">
+      <div className="container mx-auto px-4 sm:px-6 max-w-6xl p-16 text-white">
+        <header className="flex flex-col mb-4">
+          <div className="flex">
+            <img src="/logo.svg" className="mr-4 w-5 h-5" alt="TxPipe Logo" />
+            <p className="text-sm">Starter Kit provided by TxPipe</p>
+          </div>
 
-            {/* Available wallets */}
-            {wallets.length ? <>
-              <div className="grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-                { wallets.map((wallet: WalletInfo) => <WalletCard key={wallet.id} wallet={wallet} handleClick={connectWallet}/>)}
-              </div>
-            </> : <h3 className="text-3xl text-gray-200 font-extrabold mt-4">No Wallets were found</h3>}
+          <h1 className="text-4xl font-light mt-2">Wallet Connector</h1>
+          <p className="text-gray-400 mt-8">
+            Select which wallet to connect and perform basic interactions.
+          </p>
+        </header>
 
-            {/* Connected Wallet information */}
-            <div className='mt-8'>
-              { enabledWallet ? 
-              <>
-                <h3 className="text-l text-white font-extrabold mt-4">{`Connected to ${enabledWallet.name}`}</h3>
-                <h3 className="text-sm text-gray-200 mt-4">{balance ? `Wallet Balance: ${balance}` : null}</h3>
-                <h3 className="text-sm text-gray-200 mt-2">{network ? `Connected to: ${network}` : null}</h3>
-              </> : 
-              <><h3 className="text-l text-gray-400 font-extrabold mt-4">No wallet is enabled. Select a Wallet to enabled it</h3></>}
+        {/* Available wallets */}
+        {wallets.length ? (
+          <div className="grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+            {wallets.map((wallet: WalletInfo) => (
+              <WalletCard
+                key={wallet.id}
+                wallet={wallet}
+                selected={selectedWallet === wallet.id}
+                onClick={() => {
+                  setSelectedWallet(wallet.id);
+                }}
+              />
+            ))}
+          </div>
+        ) : (
+          <p className="text-red-500">No wallets were found in your browser.</p>
+        )}
+
+        {/* Connected Wallet information */}
+        <div className="mt-8">
+          {enabledWallet ? (
+            <div className="rounded-md bg-gray-600 bg-opacity-10 p-6">
+              <h3 className="text-xl font-medium tracking-wide">{`Connected to ${enabledWallet.name}`}</h3>
+              <p className="font-semibold uppercase text-txblue text-xs mt-2">
+                Wallet: <span className="text-white">{selectedWallet}</span>
+              </p>
+              <p className="font-semibold uppercase text-txblue text-xs mt-2">
+                Balance: <span className="text-white">{balance}</span>
+              </p>
+              <p className="font-semibold uppercase text-txblue text-xs mt-2">
+                Network: <span className="text-white">{network}</span>
+              </p>
             </div>
-            
-            {/* Displays any error message */}
-            { error? <>
-              <div className="mt-4 rounded-lg border border-red-500 bg-red-600 bg-opacity-10 p-4 text-white">
-                  <h1 className="font-bold">Error</h1>
-                  <h3 className="text-sm text-red-500 mt-2">{`There was an error connecting to the selected wallet: ${error}`}</h3>
-            </div></> : null }
-            
+          ) : null}
         </div>
+
+        {/* Displays any error message */}
+        {error ? (
+          <p className="text-sm text-red-500 mt-2">{`There was an error connecting to the selected wallet: ${error}`}</p>
+        ) : null}
+      </div>
     </div>
-  );  
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,36 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
-  },
+    transitionDuration: {
+        DEFAULT: '400ms',
+    },
+    extend: {
+        fontFamily: {
+            serif: ['Inter', 'sans-serif'],
+            sans: ['Inter', 'sans'],
+        },
+        colors: {
+            txpink: '#DD62FD',
+            txpink2: '#9E46B4',
+            txblue: '#0AD0FF',
+            txblue2: '#08AACF',
+            gray: {
+                50: '#fafafa',
+                100: '#f5f5f5',
+                200: '#e5e5e5',
+                300: '#d4d4d4',
+                400: '#a3a3a3',
+                500: '#737373',
+                600: '#525252',
+                700: '#404040',
+                750: '#323232',
+                800: '#272727',
+                850: '#222222',
+                900: '#1d1d1d',
+                950: '#141414',
+            },
+        },
+    },
+},
   plugins: [],
 }
